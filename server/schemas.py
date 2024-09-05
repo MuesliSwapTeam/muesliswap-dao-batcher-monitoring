f8e39e24909fce87bb641c855cbdce5d66b00193
Patrick Dowd <padowd@student.ethz.ch>
2024-09-05 13:18:54 +0200

    Added response models

--- server/schemas.py
+++ server/schemas.py
@@ -2,6 +2,25 @@ from pydantic import BaseModel
 from typing import List
 
 
-class BatcherStats(BaseModel):
+class BatcherResponse(BaseModel):
     num_transactions: int
     addresses: List[str]
+
+
+class BatcherStatsResponse(BaseModel):
+    max_profit: float
+    min_profit: float
+    avg_profit: float
+    total: float
+
+
+class ExpandedBatcherStatsResponse(BatcherStatsResponse):
+    num_transactions: int
+    addresses: List[str]
+
+
+class TransactionResponse(BaseModel):
+    tx_hash: str
+    ada_profit: int
+    non_ada_profit: float
+    other_assets: dict
