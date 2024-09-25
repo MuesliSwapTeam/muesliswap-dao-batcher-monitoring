8271df58b50dfd0f10f5cbe3f096d39c72e98c2c
muesli-blockfrost server <muesliswap@gmail.com>
2024-09-25 18:21:16 +0000

    Added caching

--- server/serve.py
+++ server/serve.py
@@ -1,12 +1,15 @@
 from fastapi import FastAPI, Depends, HTTPException
+from fastapi_cache import FastAPICache
+from fastapi_cache.backends.inmemory import InMemoryBackend
+from fastapi_cache.decorator import cache
 from sqlalchemy.orm import sessionmaker, Session
+from contextlib import asynccontextmanager
 from typing import List
 
 from . import crud
 from common.db import Batcher, _ENGINE
 from .schemas import *
 
-app = FastAPI()
 
 SessionLocal = sessionmaker(autoflush=False, autocommit=False, bind=_ENGINE)
 
@@ -19,16 +22,27 @@ def get_session():
         session.close()
 
 
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    FastAPICache.init(InMemoryBackend())
+    yield
+
+
+app = FastAPI(lifespan=lifespan)
+
+
 @app.get("/", response_model=dict)
 async def root():
     return {"message": "MuesliSwap Batcher Analytics"}
 
 
+@cache(expire=60)
 @app.get("/batchers", response_model=List[BatcherResponse])
 async def batchers(session: Session = Depends(get_session)):
     return crud.get_batchers(session)
 
 
+@cache(expire=60)
 @app.get("/stats", response_model=BatcherStatsResponse)
 async def batcher_stats(address: str, session: Session = Depends(get_session)):
     response = crud.batcher_stats(session, address)
@@ -37,11 +51,13 @@ async def batcher_stats(address: str, session: Session = Depends(get_session)):
     return response
 
 
+@cache(expire=60)
 @app.get("/all-stats", response_model=List[ExpandedBatcherStatsResponse])
 async def all_batcher_stats(session: Session = Depends(get_session)):
     return crud.all_batcher_stats(session)
 
 
+@cache(expire=60)
 @app.get("/transactions", response_model=List[TransactionResponse])
 async def batcher_transactions(address: str, session: Session = Depends(get_session)):
     response = crud.batcher_transactions(session, address)
